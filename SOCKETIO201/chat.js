ea0a52abd93e2856032b3ba87cac9b90d0f3bba8
inPhoenix <spiderk@gmail.com>
2018-11-19 07:04:29 +0000

    Starting Slack

---
+++ SOCKETIO201/chat.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const chalk = require('chalk')
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'))
+
+const expressServer = app.listen(9000)
+const io = socketio(expressServer)
+io.on('connection', socket => {
+  socket.emit('messageFromServer', { data: 'welcome to socketio server' })
+  socket.on('messageToServer', dataFromClient => {
+    console.log(dataFromClient)
+  })
+  socket.on('newMessageToServer', (msg)=> {
+    // console.log(chalk.blue(msg.text));
+    io.emit('messageToClient', {text: msg.text})
+  })
+})
+
+console.log(chalk.cyan('listening 9000'))
