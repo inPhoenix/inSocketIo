76c785402c76479de939e3778d38384e8a153e9f
inPhoenix <spiderk@gmail.com>
2018-11-21 16:16:17 +0000

    prettier

--- slack/slack.js
+++ slack/slack.js
@@ -1,58 +1,39 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const socketio = require('socket.io')
-app.use(express.static(__dirname + '/public'));
-const expressServer = app.listen(9000);
-const io = socketio(expressServer);
+app.use(express.static(__dirname + '/public'))
+const expressServer = app.listen(9000)
+const io = socketio(expressServer)
 const chalk = require('chalk')
 
 let namespaces = require('./data/namespaces')
-namespaces.forEach((namespace) => {
-  console.log(namespace.endpoint)
-  io.of(namespace.endpoint).on('connection', (socket) => {
-    console.log(`${socket.id} has join ${namespace.endpoint}`)
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // io.on = io.of('/').on
-io.on('connection',(socket)=>{
-    socket.emit('messageFromServer',{data:"Welcome to the socketio server"});
-    socket.on('messageToServer',(dataFromClient)=>{
-        console.log(dataFromClient)
-    })
-    socket.on('newMessageToServer',(msg)=>{
-        // console.log(msg)
-        // io.emit('messageToClients',{text:msg.text})
-        io.of('/').emit('messageToClients',{text:msg.text})
-    })
-    // The server can still communicate across namespaces
-    // but on the clientInformation, the socket needs be in THAT namespace
-    // in order to get the events
+io.on('connection', socket => {
+  socket.emit('messageFromServer', { data: 'Welcome to the socketio server' })
+  socket.on('messageToServer', dataFromClient => {
+    console.log(dataFromClient)
+  })
+  socket.on('newMessageToServer', msg => {
+    // console.log(msg)
+    // io.emit('messageToClients',{text:msg.text})
+    io.of('/').emit('messageToClients', { text: msg.text })
+  })
+  // The server can still communicate across namespaces
+  // but on the clientInformation, the socket needs be in THAT namespace
+  // in order to get the events
 
-    setTimeout(()=>{
-        io.of('/admin').emit('welcome',"Welcome to the admin channel, from the main channel!")
-    },2000)
-
-
-
+  setTimeout(() => {
+    io.of('/admin').emit(
+      'welcome',
+      'Welcome to the admin channel, from the main channel!'
+    )
+  }, 2000)
 })
 
-io.of('/admin').on('connection',(socket)=>{
-    console.log("Someone connected to the admin namespace!")
-    io.of('/admin').emit('welcome',"Welcome to the admin channel!");
+namespaces.forEach(namespace => {
+  console.log(namespace.endpoint)
+  io.of(namespace.endpoint).on('connection', socket => {
+    console.log(`${socket.id} has join ${namespace.endpoint}`)
+  })
 })
